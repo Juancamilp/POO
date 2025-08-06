8b06ac7e1f44720edf605f234a694fdfda02947e
Juancamilp <juan.holguinz@campusucc.edu.co>
2025-08-06 21:00:37 +0000

    Creacion de mi primer programa en java

--- Clase 1/HolaMundo.java
+++ Clase 1/HolaMundo.java
@@ -3,6 +3,12 @@ public class HolaMundo {
     public static void main(String[] args){
 
        System.out.println("Mi primer hola mundo en JAVA"); 
+    
+    //Creación de la variable x de tipo entero
+    int x = 2;
+    
+    
     }
+
 
 }
